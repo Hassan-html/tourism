bccb2a1d5400ab9b01214d6b529118eb9c98e095
HSN-CODES <logics.nook@gmail.com>
2024-05-08 12:03:34 +0500

    "tour updated"

--- app/staticdata/data.ts
+++ app/staticdata/data.ts
@@ -8,24 +8,26 @@ export const Tours = [
     mainImg: "mountfuji.jpg",
     tourLocation: "Chiyoda, Japan",
     Overview: [
-      "See and do exactly what you want on this private, customizable day tour to Mt. Fuji. This Japanese icon offers a range of natural, cultural, and adventure activities. Enjoy the views from the 5th Station, visit Shinto shrines at the mountain’s base, see one or more of the Fuji Five Lakes, and end the day at a popular outlet mall for some retail therapy.",
+      "Check out one of Japan’s most beautiful, iconic destinations—Mt. Fuji—on this convenient day tour from Tokyo. Visit several places in the Fuji area from where you can get beautiful views of the mountain (weather permitting), including Hakone, Lake Kawaguchi, the 5th Station on Mt. Fuji, and more. As this is a private tour, you can customize the itinerary to suit your own interests.",
       [
-        "As this is a private tour, the itinerary is a suggestion and customizable.",
-        "Learn more about the mountain that has shaped Japanese art and culture.",
-        "An easy way to fit many sights and activities into a single day.",
-        "This varied tour will appeal to all members of your party.",
+        "The flexible itinerary can be adjusted to suit your needs.",
+        "See as much or as little as you like during the day, as this is a private tour for you.",
+        "An easy and hassle-free way of visiting the Fuji area from Tokyo.",
+        "Travelers with a range of interests will enjoy the attractions around Mt. Fuji.",
       ],
     ],
     Timing: ["07:30", "08:00", "08:30"],
     include: [
-      "Meet & Greet",
       "Private transportation",
       "Air-conditioned vechicle",
-      "Gas/petrol",
-      "Highway/Tolls",
+      "Pick Up and Drop off to Hotel",
       "English speaking guide",
     ],
-    exclude: ["Food & beverage", "Entrance fee requires for activities"],
+    exclude: [
+      "Food & beverage",
+      "Entrance fee requires for activities",
+      "Mt Fuji enterance fee 2100¥ per group",
+    ],
     TourAmenities: [
       "fee Cancellation",
       "Language Support (English)",
@@ -36,40 +38,52 @@ export const Tours = [
     ],
     TourPlan: [
       {
-        title: "Mt. Fuji Subaru Line 5th Station",
+        title: "Mount Fuji",
         detail:
-          "The highest peak of Mt Fuji (2200m) that is accessible by car, interesting for the hikers. This is the closest point to view the top of Mt fuji. You can have a splendor view of the city from the view points.",
-        duration: "30 minutes • Admission Ticket Not Included",
-      },
-      {
-        title: "Arakurayama Sengen Park",
-        detail:
-          "Temple and park with many stairs and different stages leads to exotic and mind blowing view of Mt Fuji at the top. The paragon at the top is very famous for viewing and picture taking point.",
-        duration: "45 minutes • Admission Ticket Free",
+          "Visit Mt fuji viewing locations and Hakone at same day on this day trip from Tokyo. This tour is private charter with English speaking guide. Our guide will introduce you beautiful places with fun. The trip is fully customizable and we are open for your suggestions. Recommend itinerary Mt fuji and Hakone itinerary - lake Kawaguchi - mt fuji 5th station - Kachi Kachi ropeway - Hakone ropeway - lake ashi - owakudani Hotel drop off at 6:30. Mt Fuji area only suggested itinerary Mt fuji 5th Station if weather permitted Lake Kawaguchi Kachi Kachi ropeway Kita hongou Sengen shrine Oshino Hakkai Oishi park Arashiyam Sengen shrine if we have time or depend upon customer preferences. Please understand you have to select either 1 itinerary.",
+        duration: "1 hour • Admission Ticket Included",
       },
       {
         title: " Lake Kawaguchiko",
         detail:
-          "Lake kawaguchi is the second largest lake of the Mt Fuji five lakes, can enjoy the beautiful view of Mt fuji The Kachi Kachi Ropeway ascends to an observation point near the summit of Mount Tenjo, from where Lake Kawaguchiko and Mount Fuji can be seen. There is also many Hot spring available in the area where you can calm yourself.",
-        duration: "2 hours • Admission Ticket Not Included",
+          "2nd Biggest Lake of Mt Fuji area after Lake Yamanaka. You can see Mt Fuji from Kachi Kachi ropeway or cruise at Lake Kawaguchi.",
+        duration: "1 hour • Admission Ticket not included",
       },
       {
-        title: "Oishi Park",
+        title: " Mt. Fuji 5th Station",
         detail:
-          "Oishi Park is a public park located on the north shore of Lake Kawaguchi. From the park you can enjoy a beautiful view of Mt. Fuji and Lake Kawaguchi. The park features a 350m walking path with many flowers planted along it; from early April to Late May the moss phlox flowers spread out like a pink carpet, from late June into late July the gorgeous lavender plants bloom, and from mid to late-October the kokia shrubs show their beautiful red autumn colors. The contrast between the various flowers and Mt. Fuji is spectacular. The park is also home to the Kawaguchiko Natural Living Center where you can enjoy making fruit jam and an all-you-can eat cherry & blueberry picking course. They also feature a café where you can try specialty made blueberry ice cream.",
+          "The Last Point at Mt fuji where we can reach by Car. Please understand weather conditions. During hiking Season we cannot go to 5th station. You have to take shuttle bus cost 2100¥ person.",
         duration: "30 minutes • Admission Ticket Not Included",
       },
       {
-        title: "Hakkai",
+        title: "Lake Ashinoko",
         detail:
-          "A small village which is a famous tourists attraction due to the presence of 8 ponds; the fresh water from Mt fuji come down in whose ponds. Also famous for the vendors of foods and souvenirs.",
-        duration: "45 minutes • Admission Ticket Not Included",
+          "Lake Ashi, also referred to as Hakone Lake or Ashinoko Lake, is a scenic lake in the Hakone area of Kanagawa Prefecture in Honshū, Japan. You can Experience Pirate Cruise at Lake Ashi. If we take mt fuji and Hakone itinerary.",
+        duration: "1 hour • Admission Ticket Not Included",
       },
       {
-        title: "Gotemba Premium Outlets",
+        title: "Oshino Hakkai",
         detail:
-          "The shopping mall with all the premium outlets of major brands. Can go their if have time.",
+          "A small village which is a famous tourists attraction due to the presence of 8 ponds; the fresh water from Mt fuji come down in whose ponds. Also famous for the vendors of foods and souvenirs.",
+        duration: "1 hour • Admission Ticket Not Included",
+      },
+      {
+        title: "Kitaguchi Hongu Fuji Sengen Jinja Shrine",
+        detail:
+          "In the past, Fujiyoshida's Sengen Shrine used to be the common starting point for climbing Mount Fuji from the north.",
         duration: "1 hour • Admission Ticket Free",
+      },
+      {
+        title: "Arakurayama Sengen Park",
+        detail:
+          "Iconic pagoda on a hilltop facing Mt. Fuji in Arakurayama Sengen Park, reached via 398 steps.",
+        duration: "1 hour • Admission Ticket Free",
+      },
+      {
+        title: "Saiko Iyashi no Sato Nemba",
+        detail:
+          "A traditional Japanese village. If we get time during 10 hour trip and you take mt fuji surrounding's area itinerary.",
+        duration: "1 hour • Admission Ticket not included",
       },
     ],
   },
@@ -113,37 +127,43 @@ export const Tours = [
         title: "Senso-ji Temple",
         detail:
           "Tokyo`s oldest and most prominent temple. The majestic Senso-Ji Buddhist Temple is almost 1400 years old and is a symbol of Asakusa and the entire city of Tokyo.",
-        duration: "50 minutes • Admission Ticket Not Included",
+        duration: "50 minutes • Admission Ticket free",
       },
       {
         title: " Tokyo Skytree",
         detail:
           "Tokyo Skytree is the world`s tallest tower at 634m and the second tallest structure in the world standing head and shoulders above everything else in the city for a spectacular view.",
-        duration: "1 hour • Admission Ticket Free",
+        duration: "1 hour • Admission Ticket not included",
       },
       {
         title: " The East Gardens of the Imperial Palace (Edo Castle Ruin)",
         detail:
           "The Imperial Palace East Gardens are part of the inner Palace area. They are the former site of Edo Castle`s innermost circles of defence. A Majestic site.",
-        duration: "40 minutes • Admission Ticket Not Included",
+        duration: "40 minutes • Admission Ticket free",
       },
       {
         title: "Tsukiji Fish Market",
         detail:
           "One of the best fish markets in the world, Tsukiji Outer Market has a fantastic atmosphere. A wide array of street food stalls and excellent restaurants are available -serving both raw and cooked fish. Lunch should be enjoyed here (paid at own expense).",
-        duration: "1 hour • Admission Ticket Not Included",
+        duration: "1 hour • Admission Ticket free",
       },
       {
         title: "Meiji Jingu Shrine",
         detail:
           "(Optional Yoyogi Park and Takeshita Walking Street) - Meiji Shrine is Tokyo's most prominent shrine and dedicated to the deified spirits of Emperor Meiji and Empress Shoken, completed in 1920. Emperor Meiji was the first Emperor of modern Japan. The beautiful Yoyogi Park and Takeshita Street (observe Japanese youth, cosplay and street culture) are optional!",
-        duration: "45 minutes • Admission Ticket Not Included",
+        duration: "45 minutes • Admission Ticket free",
       },
       {
         title: "Shibuya Crossing",
         detail:
           "The busiest intersection in the world and a world-famous landmark of Tokyo city. A must-see spot.",
         duration: "30 minutes • Admission Ticket Free",
+      },
+      {
+        title: "Shinjuku Gyoen National Garden",
+        detail:
+          "Shinjuku Gyoen was constructed on the site of a private mansion belonging to Lord Naito, a 'daimyo' (feudal lord) of the Edo era. Completed in 1906 as an imperial garden, it was re-designated as a national garden",
+        duration: "30 minutes • Admission Ticket not included",
       },
     ],
   },
@@ -183,9 +203,39 @@ export const Tours = [
     ],
     TourPlan: [
       {
-        title: "Dokotrip",
-        detail:
-          "You will be picked up from your hotel in central Tokyo and be taken to the sites, in the type of vehicle of your choice. This 1 Day Nikko is fully customizable, bringing you and your group to some of the most beautiful and important stops in the region, from the Toshogu Shrine Complex, to gorgeous Kegon Falls and Lake Chuzenji. A tradtional and delicious Nikko style meal, the yuba gozen, is recommended. However, you may opt for your own choice too. The drive along the Japan Romantic Road is an experience in itself. All with our expert, English speaking driver with you every step of the way.   Here is a suggested itinerary: 8:00 a.m - Pick up from hotel in Central Tokyo area   - Toshogu Shrine Complex - Shinkyo Bridge (Red Bridge) - Nikko Style Lunch - Kegon Falls - Lake Chuzenji 7:00 p.m -  Drop off at hotel in Central Tokyo area *NOTE: This is not a guided tour. Our drivers, however, speak English well, are able to be simple guides and know the areas/routes expertly.",
+        title: "Akechidaira Ropeway Observation Deck",
+        detail: "",
+        duration: "-- • Admission Ticket Not Included",
+      },
+      {
+        title: " Chuzenji Ferry Terminal",
+        detail: "",
+        duration: "-- • Admission Ticket Not Included",
+      },
+      {
+        title: "Lake Chuzenji Parking",
+        detail: "",
+        duration: "-- • --",
+      },
+      {
+        title: "Kegon Falls",
+        detail: "",
+        duration: "-- • Admission Ticket Not Included",
+      },
+      {
+        title:
+          " Lunch in Chuzenji Lake Area Traditional Japanese Food Near Lake",
+        detail: "",
+        duration: "-- • Admission Ticket Not Included",
+      },
+      {
+        title: "Nikko Tamozawa Imperial Villa Memorial Park.",
+        detail: "",
+        duration: "-- • Admission Ticket Not Included",
+      },
+      {
+        title: "Nikko Tosho-gu Shrine",
+        detail: "",
         duration: "-- • Admission Ticket Not Included",
       },
     ],
@@ -194,7 +244,7 @@ export const Tours = [
     Id: 4,
     Title: " Tokyo Narita Airport (NRT) Transfer Private Place",
     Duration: "1:30 hours",
-    Price: 360,
+    Price: 215,
     mainImg: "airport.jpg",
     tourLocation: "Yaesu, Japan",
     Overview: [
@@ -227,6 +277,54 @@ export const Tours = [
         title: "What To Expect",
         detail:
           " We will contact you to confirm the pick-up details in advance. And we will arrange the driver one day in advance and send the driver's info to you.The driver will wait for you at the pick up point, our customer service will help you to meet the driver when you arrive. We also will trace your flight information even your flight is delay.Please note that this reservation is only for one way. If you want to book the transfer for the return trip, please make another booking or contact us.We provide 7*24 customer service, please feel free to contact us by email, WhatsApp or phone. ",
+        duration: "",
+      },
+    ],
+  },
+  {
+    Id: 4,
+    Title:
+      " One Day Tour Snow Monkey Park and Zenkoji Temple With English Speaking Guide",
+    Duration: "12 hours",
+    Price: 490,
+    mainImg: "snow.jpg",
+    tourLocation: "Yaesu, Japan",
+    Overview: [
+      "A tour to the Snow Monkey Park and Zenkoji Temple offers a captivating blend of natural beauty, cultural richness, and spiritual enlightenment. Here's an overview of what you can expect from this enchanting journey",
+      [
+        "Scenic Beauty: Traverse through stunning landscapes adorned with snow-capped mountains, dense forests, and winding rivers, offering postcard-perfect views at every turn.",
+        "Wildlife Encounter: Observe the endearing antics of Japanese macaques as they frolic and socialize in the natural hot springs, providing a unique opportunity for wildlife photography and observation.",
+        "Cultural Immersion: Immerse yourself in the rich tapestry of Japanese culture and spirituality as you explore the ancient halls, sacred relics, and rituals of Zenkoji Temple, guided by knowledgeable local experts.",
+        "Relaxation and Reflection: Take a moment to unwind and reflect amidst the tranquil ambiance of the temple grounds, fostering a sense of inner peace and mindfulness.",
+      ],
+    ],
+    Timing: ["07:30", "08:00", "08:30"],
+    include: ["Private transportation", "Air-conditioned vechicle"],
+    exclude: [
+      "Additional costs beyond Tokyo Central City",
+
+      "Additional costs beyond Tokyo Central City",
+      "Fee for the meet & greet in the arrival hall",
+    ],
+    TourAmenities: [
+      "fee Cancellation",
+      "Language Support (English)",
+      "Low Price",
+      "Mobile Tickets",
+      "Pay Later",
+      "Pickup Offered",
+    ],
+    TourPlan: [
+      {
+        title: "Snow Monkey Park",
+        detail:
+          "Nestled in the picturesque landscapes of the Japanese Alps, the Snow Monkey Park (Jigokudani Yaen-Koen) is a sanctuary for Japanese macaques, also known as snow monkeys. Here, amidst steaming hot springs and snow-laden forests, you'll witness these adorable creatures soaking in the warmth of the natural hot springs, especially during the winter months. It's a rare and heartwarming experience to observe them playing, grooming, and relaxing in their natural habitat, offering an intimate glimpse into their daily lives. ",
+        duration: "",
+      },
+      {
+        title: "Zenkoji Temple",
+        detail:
+          "As one of Japan's most revered Buddhist temples, Zenkoji Temple holds deep cultural and spiritual significance. Located in Nagano city, the temple's history dates back over 1,400 years, making it a center of pilgrimage and devotion for centuries. Upon entering the temple precincts, you'll be struck by the serene atmosphere and architectural splendor of its main hall, which houses the Amida Buddha, a revered national treasure. Visitors can participate in traditional rituals, such as chanting and incense offerings, and explore the temple's various halls, gardens, and artifacts, each steeped in profound symbolism and religious tradition.",
         duration: "",
       },
     ],
