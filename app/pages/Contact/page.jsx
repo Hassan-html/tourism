bb2e896fbe30890a442bfba8f2c70fc148e1c299
HSN-CODES <logics.nook@gmail.com>
2024-05-11 15:58:32 +0500

    "ui fixed"

--- app/pages/Contact/page.jsx
+++ app/pages/Contact/page.jsx
@@ -195,7 +195,7 @@ export default function App() {
                   }}
                   required
                 >
-                  <option value="">Group of 5 persons</option>
+                  <option value="">5 person per Group</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
