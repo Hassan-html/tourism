55b2c921a222a2fc93bc4c4997292f1132edd8b6
LogicNook <121165697+Hassan-html@users.noreply.github.com>
2024-05-14 17:22:56 +0500

    Update page.jsx

--- app/pages/Contact/page.jsx
+++ app/pages/Contact/page.jsx
@@ -143,7 +143,7 @@ export default function App() {
                   });
                 }}
                 className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
+               
                 required
               />
             </div>
